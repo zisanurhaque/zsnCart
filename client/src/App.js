b8c47bcc7d71f5eee0dc66322b2845322f4e07bd
Zisanur Haque <zisanurhaqueag@gmail.com>
2022-06-05 01:00:27 +0600

    5-6-2022 1:00AM

---
+++ client/src/App.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const App = () => {
+
+  const [products, setProducts] = useState([])
+  const [cartItem, setCartItem] = useState([])
+  const [toggle, setToggle] = useState(true)
+
+  const handleCart = (id) => {
+    const value = {
+      id: id
+    }
+    axios.post('https://zsncart.herokuapp.com/addtocart', value)
+  }
+
+  const handleDelete = (id) => {
+    axios.delete(`https://zsncart.herokuapp.com/deleteItem/${id}`)
+  }
+
+  const handleQty = (value) => {
+    if(value.type === "inc"){
+      axios.put("https://zsncart.herokuapp.com/increment", value)
+    }
+    else if(value.type === "dec"){
+      axios.put("https://zsncart.herokuapp.com/decrement", value)
+    }
+  }
+
+  const handleToggle = (e) => {
+    e.preventDefault()
+    setToggle(!toggle)
+  }
+
+  useEffect(() => {
+
+    fetch("https://zsncart.herokuapp.com/getProducts", {
+      method: "GET"
+    }).then((response) => response.json()).then(response => setProducts(response))
+
+    fetch("https://zsncart.herokuapp.com/getCartItems", {
+      method: "GET"
+    }).then((response) => response.json()).then(response => setCartItem(response))
+
+  }, [cartItem])
+
+  return (
+    <>
+        <div className='container'>
+          
+          <button className='toggle' onClick={(e) => handleToggle(e)}>
+            Cart({cartItem?.length})
+          </button>
+          <div className='product-page'>
+            <h5>Products</h5>
+            {
+              products.map((product, index) => (
+                <div className='card' key={index}>
+                  <h4 className='title'>
+                    {product.name}
+                  </h4>
+                  <h6 className='price'>{product.currency}{product.price}</h6>
+                  <button onClick={() => {
+                    handleCart(product._id)
+                  }}>Add To Cart</button>
+                </div>
+              ))
+            }
+          </div>
+          {
+            !toggle ? "" : 
+            <div className='cart-page'>
+            <h4>Shopping Cart</h4>
+            <div className='list header'>
+              <p className='name'>Course Name</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total Price</p>
+              <p className='delete'>X</p>
+            </div>
+            {
+              cartItem.map((item, index) => (
+                <div className='list' key={index}>
+                  <p className='name'>{item.name}</p>
+                  <p className='price'>{item.currency}{item.price}</p>
+                  <p className='counter'>
+                      <button onClick={() => handleQty({
+                        id: item._id,
+                        type: "dec"
+                      })}>-</button>
+                        <span>{item.qty}</span>
+                      <button onClick={() => handleQty({
+                        id: item._id,
+                        type: "inc"
+                      })}>+</button>
+                  </p>
+                  <p>{item.currency}{item.total}</p>
+                  <button className='delete' onClick={() => handleDelete(item._id)}>X</button>
+                </div>
+              ))
+            }
+            {
+              cartItem?.length === 0 ? "" : 
+              <div className='list footer'>
+                <p className='name'></p>
+                <p className='price'></p>
+                <p className='counter'>Total Price</p>
+                <p>
+                  {
+                    cartItem?.reduce((a, b) => {
+                      return a + b.total
+                    }, 0)
+                  }
+                </p>
+                <p className='delete'></p>
+              </div>
+            }
+          </div>
+          }
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const { urlencoded } = require('express')
+const cors = require('cors')
+const path = require('path')
+
+// middlewares
+dotenv.config({path: './config.env'})
+app.use(express.json())
+app.use(urlencoded({extended: true}))
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    credentials: true
+}))
+app.use(cookieParser())
+
+// Config File
+
+const PORT = process.env.PORT
+const DB = process.env.DB
+const SECRET = process.env.SECRET
+
+// Database Connection
+
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("database connected successfuly")
+}).catch((err) => {
+    console.log("database connection error")
+})
+
+// Product Schema
+
+const product = mongoose.Schema({
+    name: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    currency: {
+        type: String
+    }
+})
+
+const ProductSchema = mongoose.model("PRODUCT", product)
+
+// Cart Schema
+
+const cart = mongoose.Schema({
+    name: {
+        type: String
+    },
+    qty: {
+        type: Number
+    },
+    price: {
+        type: Number
+    },
+    currency: {
+        type: String
+    },
+    total: {
+        type: Number
+    }
+})
+
+const CartSchema = mongoose.model("CART", cart)
+
+app.post("/addtocart", async(req, res) => {
+    const {id} = req.body
+    const data = await ProductSchema.findById({_id: id})
+    const cartData = await CartSchema.find({name: data.name, price: data.price})
+    if(cartData.length !== 0){
+        
+    }else{
+        const cart = await CartSchema.create({
+            name: data.name,
+            price: data.price,
+            qty: 1,
+            currency: data.currency,
+            total: data.price
+        })
+        cart.save()
+        res.json({cart})
+    }
+})
+
+app.get("/getProducts", async(req, res) => {
+    const data = await ProductSchema.find()
+    res.send(data)
+})
+
+app.get("/getCartItems", async(req, res) => {
+    const data = await CartSchema.find()
+    res.json(data)
+})
+
+app.delete("/deleteItem/:id", async(req, res) => {
+    const {id} = req.params
+    const data = await CartSchema.findOneAndDelete({_id: id})
+    res.json({data})
+})
+
+app.put("/increment", async(req, res) => {
+    const {id} = req.body
+    const data = await CartSchema.findById({_id: id})
+    const newQty = data.qty + 1
+    const newTotal = newQty * data.price
+    const update  = await CartSchema.findByIdAndUpdate({_id: id}, {qty: newQty, total: newTotal})
+    res.json({update})
+})
+
+app.put("/decrement", async(req, res) => {
+    const {id} = req.body
+    const data = await CartSchema.findById({_id: id})
+    const newQty = data.qty <= 0 ? 0 : data.qty - 1
+    const newTotal = newQty * data.price
+    const update  = await CartSchema.findByIdAndUpdate({_id: id}, {qty: newQty, total: newTotal})
+    res.json({update})
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+// Server Listener
+
+app.listen(PORT, () => {
+    console.log("Server is running...")
+})
